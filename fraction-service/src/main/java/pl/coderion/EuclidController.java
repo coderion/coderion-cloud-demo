592051077139dd5ddae43ae8d0fa552315c1dabc
toudidel <toudidel@gmail.com>
2020-10-26 20:57:51 +0100

    Added parametrized fault tolerance examples

--- fraction-service/src/main/java/pl/coderion/EuclidController.java
+++ fraction-service/src/main/java/pl/coderion/EuclidController.java
@@ -1,7 +1,9 @@
 package pl.coderion;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.math3.random.RandomDataGenerator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,25 @@ public class EuclidController {
     @HystrixCommand(fallbackMethod = "fallbackGcd")
     public Integer faultToleranceExample() {
         throw new RuntimeException("fault-tolerance-example");
+    }
+
+    @GetMapping("/gcd-fault/{a}/{b}")
+    @HystrixCommand(fallbackMethod = "fallbackGcd", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "10"),  // timeout 10ms
+            @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "10000"),        // 10s window
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),              // 5 times
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),           // min 50% calls with error
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000"),        // leave 5s in fallback
+    })
+    public Integer getGcdWithFaultTolerance(@PathVariable Integer a, @PathVariable Integer b) {
+        try {
+            Thread.sleep(new RandomDataGenerator().nextLong(1, 20));
+            Integer gcd = Euclid.gcd(a, b);
+            log.info(String.format("GCD(%s,%s)=%s", a, b, gcd));
+            return gcd;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @GetMapping("/gcd/{a}/{b}")
@@ -30,7 +51,7 @@ public class EuclidController {
         return lcd;
     }
 
-    public Integer fallbackGcd() {
+    public Integer fallbackGcd(Integer a, Integer b) {
         return Euclid.gcd(1, 1);
     }
 }

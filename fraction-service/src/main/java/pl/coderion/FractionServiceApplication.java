5418d9d223d4ed3c13ac676ee46548496ddc350b
toudidel <toudidel@gmail.com>
2020-10-26 15:28:06 +0100

    Added Sleuth & Zipkin

--- fraction-service/src/main/java/pl/coderion/FractionServiceApplication.java
+++ fraction-service/src/main/java/pl/coderion/FractionServiceApplication.java
@@ -1,12 +1,19 @@
 package pl.coderion;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class FractionServiceApplication {
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FractionServiceApplication.class, args);

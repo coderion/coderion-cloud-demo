bd4f6a6b788644beec416f8f52ffc367d52c3f7a
toudidel <toudidel@gmail.com>
2020-10-28 13:32:08 +0100

    Externalized service configurations

--- run-local-with-hystrix.sh
+++ run-local-with-hystrix.sh
@@ -15,29 +15,29 @@ echo "build"
 $M2/mvn package > logs/build.log
 
 echo "config-server 8888"
-java -Dspring.profiles.active=local -jar config-server/target/config-server-*.jar >> logs/config-server.log &
+java -Dspring.profiles.active=local -jar config-server/target/config-server-*.jar > logs/config-server.log &
 sleep 5
 
 echo "eureka-server 8761"
-java -Dspring.profiles.active=local -jar eureka-server/target/eureka-server-*.jar >> logs/eureka-server.log &
+java -Dspring.profiles.active=local -jar eureka-server/target/eureka-server-*.jar > logs/eureka-server.log &
 sleep 5
 
 echo "fraction-service 8001"
-java -Dspring.profiles.active=local -jar fraction-service/target/fraction-service-*.jar >> logs/fraction-service.log &
+java -Dspring.profiles.active=local -jar fraction-service/target/fraction-service-*.jar > logs/fraction-service.log &
 sleep 5
 
 echo "math-service 8101"
-java -Dspring.profiles.active=local -jar math-service/target/math-service-*.jar >> logs/math-service.log &
+java -Dspring.profiles.active=local -jar math-service/target/math-service-*.jar > logs/math-service.log &
 sleep 5
 
 echo "zuul-server 8765"
-java -Dspring.profiles.active=local -jar zuul-server/target/zuul-server-*.jar >> logs/zuul-server.log &
+java -Dspring.profiles.active=local -jar zuul-server/target/zuul-server-*.jar > logs/zuul-server.log &
 sleep 5
 
 echo "hystrix-dashboard-app 7979"
-java -Dspring.profiles.active=local -jar hystrix-dashboard-app/target/hystrix-dashboard-app-*.jar >> logs/hystrix-dashboard-app.log &
+java -Dspring.profiles.active=local -jar hystrix-dashboard-app/target/hystrix-dashboard-app-*.jar > logs/hystrix-dashboard-app.log &
 sleep 5
 
 echo "turbine-server 8989"
-java -Dspring.profiles.active=local -jar turbine-server/target/turbine-server-*.jar >> logs/turbine-server.log &
+java -Dspring.profiles.active=local -jar turbine-server/target/turbine-server-*.jar > logs/turbine-server.log &
 sleep 5
--- run-local.sh
+++ run-local.sh
@@ -15,21 +15,21 @@ echo "build"
 $M2/mvn package > logs/build.log
 
 echo "config-server 8888"
-java -Dspring.profiles.active=local -jar config-server/target/config-server-*.jar >> logs/config-server.log &
+java -Dspring.profiles.active=local -jar config-server/target/config-server-*.jar > logs/config-server.log &
 sleep 5
 
 echo "eureka-server 8761"
-java -Dspring.profiles.active=local -jar eureka-server/target/eureka-server-*.jar >> logs/eureka-server.log &
+java -Dspring.profiles.active=local -jar eureka-server/target/eureka-server-*.jar > logs/eureka-server.log &
 sleep 5
 
 echo "fraction-service 8001"
-java -Dspring.profiles.active=local -jar fraction-service/target/fraction-service-*.jar >> logs/fraction-service.log &
+java -Dspring.profiles.active=local -jar fraction-service/target/fraction-service-*.jar > logs/fraction-service.log &
 sleep 5
 
 echo "math-service 8101"
-java -Dspring.profiles.active=local -jar math-service/target/math-service-*.jar >> logs/math-service.log &
+java -Dspring.profiles.active=local -jar math-service/target/math-service-*.jar > logs/math-service.log &
 sleep 5
 
 echo "zuul-server 8765"
-java -Dspring.profiles.active=local -jar zuul-server/target/zuul-server-*.jar >> logs/zuul-server.log &
+java -Dspring.profiles.active=local -jar zuul-server/target/zuul-server-*.jar > logs/zuul-server.log &
 sleep 5

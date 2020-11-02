a5c04693349e1e001d3624106acbd71e2483f1d9
toudidel <toudidel@gmail.com>
2020-11-02 14:52:05 +0100

    spring cloud security

--- auth-service/src/main/java/pl/coderion/config/mongodb/MongoBeeConfig.java
+++ auth-service/src/main/java/pl/coderion/config/mongodb/MongoBeeConfig.java
@@ -2,6 +2,7 @@ package pl.coderion.config.mongodb;
 
 import com.github.mongobee.Mongobee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
@@ -11,6 +12,15 @@ import pl.coderion.MongoProperties;
 @Configuration
 @DependsOn("mongoTemplate")
 public class MongoBeeConfig {
+
+    @Value("${spring.data.mongodb.host}")
+    private String mongoHost;
+
+    @Value("${spring.data.mongodb.port}")
+    private String mongoPort;
+
+    @Value("${spring.data.mongodb.database}")
+    private String mongoDatabase;
 
 //    private static final String MONGODB_URL_FORMAT = "mongodb://%s:%s@%s:%d/%s";  // version with username & password
     private static final String MONGODB_URL_FORMAT = "mongodb://%s:%d/%s";
@@ -25,8 +35,8 @@ public class MongoBeeConfig {
     @Bean
     public Mongobee mongobee() {
 
-        System.out.println("Host: " + mongoProperties.getHost());
-        System.out.println("Database: " + mongoProperties.getDatabase());
+        System.out.println("Host: " + mongoHost);
+        System.out.println("Database: " + mongoDatabase);
 
         Mongobee runner = new Mongobee(String.format(MONGODB_URL_FORMAT,
 //                mongoProperties.getUsername(),
